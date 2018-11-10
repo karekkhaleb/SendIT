a5c8e83779d4003685f0485154551c07fef78175
karekkhaleb <karekkhaleb@github.com>
2018-11-10 13:05:51 +0200

    updated the port variable

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import parcelsRoutesV1, { myParcels } from './routes/parcelsRoutesV1';
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const app = express();
 
 // middleware
