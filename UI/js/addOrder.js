ab4cb8ae4a9c0b4e0c2d5eda1c175f3fbea28943
Buhungiro Caleb <karekkhaleb@gmail.com>
2018-11-20 13:46:22 +0200

    Revert "I have completed the UI templates and I am waiting for the review and feedback"

--- UI/js/addOrder.js
+++
@@ -1,9 +0,0 @@
-window.onload = () =>{
-    const btns = document.querySelectorAll('.js-add-order');
-    btns.forEach((btn) => {
-        btn.addEventListener('click', (e) => {
-            window.location.href = "/create-order.html";
-            // alert('added the order');
-        });
-    });
-};
