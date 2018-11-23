7b61d85e4ff708eb3f9be84fc2697c3a68b92534
karekkhaleb <karekkhaleb@github.com>
2018-11-23 14:45:25 +0200

    before feedback session

--- src/app.js
+++ src/app.js
@@ -2,9 +2,45 @@
 import express from 'express';
 import parcelsRoutesV1 from './routes/parcelsRoutesV1';
 import ParcelsCollection from './classes/parcelsDataStructure';
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
+
+dotenv.config();
 
 const port = process.env.PORT || 9000;
 const app = express();
+const pool = new Pool();
+
+
+const connect = async () => pool.connect();
+
+const execute = async () => {
+  // const connection = await connect();
+  const query='SELECT * FROM DATABASE test'
+  const connection = await connect();
+
+  try{
+    return await connection.query(query);
+  }catch(error){
+    console.log('this is an error');
+    console.log(error);
+  }finally{
+    connection.release();
+  }
+
+  // console.log(connection);
+
+  // connect().then((res) => {
+  //   console.log(res);
+  // }).catch((err) => {
+  //   console.log(err);
+  // });
+  // console.log(connection);
+}
+
+execute();
+
+
 
 // middleware
 app.use(express.json());
@@ -56,5 +92,6 @@ app.get('/api/v1/users/:userId/parcels', (req, res) => {
 
 
 
+console.log(process.env.NAME);
 // eslint-disable-next-line no-console
 app.listen(port, () => console.log(`the app started on port ${port}`));
