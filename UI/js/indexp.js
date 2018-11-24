906433ca6dbb77be26bbb2b6735f779d3f48f09d
Caleb Buhungiro <karekkhaleb@gmail.com>
2018-11-24 10:39:25 +0200

    ft-update-homepage-and-forms

--- UI/js/indexp.js
+++ UI/js/indexp.js
@@ -1,10 +1,10 @@
-const createParcelBtn = document.querySelector('#createp-btn');
-const createParcelForm = document.querySelector('.createp-form');
-const cancelCreateForm = document.querySelector('#close-create-form');
+// const createParcelBtn = document.querySelector('#createp-btn');
+// const createParcelForm = document.querySelector('.createp-form');
+// const cancelCreateForm = document.querySelector('#close-create-form');
 
-createParcelBtn.addEventListener('click', () => {
-  createParcelForm.style.display = 'block';
-})
-cancelCreateForm.addEventListener('click', () => {
-  createParcelForm.style.display = 'none';
-})
+// createParcelBtn.addEventListener('click', () => {
+//   createParcelForm.style.display = 'block';
+// })
+// cancelCreateForm.addEventListener('click', () => {
+//   createParcelForm.style.display = 'none';
+// })
