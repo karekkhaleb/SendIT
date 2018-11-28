5d2cf0617d454a6bff9e783ecaba3ce4ff78a1ed
Caleb Buhungiro <karekkhaleb@gmail.com>
2018-11-28 23:39:03 +0200

    [Chore] add controllers

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,40 +1,11 @@
 /* eslint-disable consistent-return */
 import express from 'express';
-import db from '../db/conn';
+import authController from '../controllers/authController';
 
 const Router = express.Router();
 
-Router.post('/signup', async (req, res) => {
-  if (!req.body.email || !req.body.password) {
-    return res.status(400).json({ message: 'No email or password' });
-  }
-  const authData = {
-    email: req.body.email,
-    password: req.body.password,
-  };
-  const signedUp = db.signup(authData);
-  if (signedUp) res.status(200).json({ message: 'User signed up' });
-  else res.status(500).json({ message: 'unable to signup' });
-});
+Router.post('/signup', authController.signup);
 
-Router.post('/signin', async (req, res) => {
-  if (!req.body.email || !req.body.password) {
-    return res.status(400).json({ message: 'No email or password' });
-  }
-  const authData = {
-    email: req.body.email,
-    password: req.body.password,
-  };
-  const signedIn = await db.signin(authData);
-  if (signedIn && signedIn.token) {
-    return res.status(200).json({
-      message: 'User signed in',
-      token: signedIn.token,
-    });
-  } if (signedIn && signedIn.message) {
-    return res.status(500).json({ message: signedIn.message });
-  }
-  res.status(500).json({ message: 'unable to signin' });
-});
+Router.post('/signin', authController.signin);
 
 export default Router;
